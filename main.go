268e867659c18d5754bb55b96ef841994d92ffb6
Prajesh Pratap Singh <prajesh.eleven118@gmail.com>
2024-02-16 17:59:23 +0530

    added router

--- main.go
+++ main.go
@@ -3,17 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", handler)
+	r.HandleFunc("/books/{title}/page/{page}", getBook)
 
 	fs := http.FileServer(http.Dir("static/"))
 
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":80", r)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, you requested %s\n", r.URL.Path)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, "You're reading %s on page %s", vars["title"], vars["page"])
 }
